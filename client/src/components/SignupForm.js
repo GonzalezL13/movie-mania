812ee8ec3cc87567331efaf9c0ee57bc4d5f6cca
heatherzen <heather.zen@gmail.com>
2020-10-29 16:52:44 -0700

    forgot to save changes for last commit

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -7,4 +7,34 @@ const SignupForm = () => {
     const[userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
     const [validated] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserFormData({...userFormData, [name]: value });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        try {
+            const response = await createUser(userFormData);
+
+            if(!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const { token, user } = await response.json();
+            console.log(user);
+            Auth.login(token);
+        } catch (err) {
+            console.error(err);
+            setShowAlert(true);
+        }
+    }
 }
