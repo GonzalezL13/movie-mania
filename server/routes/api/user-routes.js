cb14459026d37ddb9c531265d38960ed9908419a
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-29 20:11:01 -0700

    updated api endpoints

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,20 @@
-
 const router = require("express").Router();
-//Update accordingly
 
-// const {
-//   getAllUsers,
-//   getUserById,
-//   createUser,
-//   updateUser,
-//   deleteUser,
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 //   addFriend,
 //   deleteFriend,
-// } = require("../../controllers/user-controller");
+} = require("../../controllers/user-controller");
 
-// //set up GET all and POST at api/users
-// router.route("/").get(getAllUsers).post(createUser);
+// set up GET all and POST at api/users
+router.route("/").get(getAllUsers).post(createUser);
 
 // //Set up GET one, PUT, and DELETE at api/users/:id
-// router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 // // set up POST and DELETE for friends api/users/userId/friends/friendsId
 // router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 // const path = require('path');
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
 
-router.use('./api', apiRoutes);
+router.use("./api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send("<h1>😝 404 Error!</h1>");
+});
 
 // router.use((req, res) =>
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"))
