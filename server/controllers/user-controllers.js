ef609384aa5ceb97bbe5d0fc5a660f5461f1379a
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-29 19:54:56 -0700

    added delete and put routes

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -40,6 +40,32 @@ const userController = {
         res.status(400).json(err);
       });
   },
+
+  //update a user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true }) //new returns updated document
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //delete a user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = userController;
