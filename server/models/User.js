f873c2e49665eb1351bd5841a548f54db92975e3
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-30 19:38:19 -0700

    added savedMovies array to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,28 +1,42 @@
 const { Schema, model } = require("mongoose");
+const movieSchema = require("./Movie");
 //require bcrypt
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+    },
+    savedMovies: [movieSchema],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 // hash user password using bcrypt
-// -- code here -- 
+// -- code here --
+
+//show number of movies saved by a user
+userSchema.virtual("movieCount").get(function () {
+  return this.savedMovies.length;
+});
 
 const User = model("User", userSchema);
 
