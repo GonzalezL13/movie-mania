b113fd7e8c6613f59af4a536a85fac43452d3acd
heatherzen <heather.zen@gmail.com>
2020-10-31 11:59:31 -0700

    changed app.js to use react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 // import ApolloClient from 'apollo-boost';
 // import './App.css';
 import Axios from 'axios';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //backend conntection to server - graphql
 // const client = new ApolloClient({
@@ -21,24 +23,18 @@ function App() {
   });
 
   return (
-    // <ApolloProvider client={client}>
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    // </ApolloProvider>
+    
+    <Router>
+      <>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={SearchMovies} />
+          <Route exact path='/saved' component={SavedMovies} />
+          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+        </Switch>
+      </>
+    </Router>
+    
   );
 }
 
