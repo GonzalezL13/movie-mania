580b3c144a02e45abb90e6a7d7490ed0a4295a3e
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-30 19:16:03 -0700

    updated movie model

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,7 +1,21 @@
 const { Schema } = require('mongoose');
 
 const movieSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+    },
+    plot: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    }
 });
 
 module.exports = movieSchema;
