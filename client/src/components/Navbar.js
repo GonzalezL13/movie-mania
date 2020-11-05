2cf8bdf00e49fcb2489b4701b356b1c4eeb1d177
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-11-04 18:17:49 -0700

    updated nav items that show when logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,73 +1,106 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
-import Auth from '../utils/auth';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
+import SignUpForm from "./SignupForm";
+import LoginForm from "./LoginForm";
+import Auth from "../utils/auth";
 
 const AppNavbar = () => {
-    const [displayModal, setDisplayModal] = useState(false);
+  const [displayModal, setDisplayModal] = useState(false);
 
-    return (
-        <>
-            <Navbar block style={{backgroundColor: '#217DA8'}} expand='lg'>
-                <Container fluid>
-                    <Navbar.Brand as={Link} to='/' block style={{color: '#1ACDC8', fontWeight: 'bold'}}>
-                        Movie Mania Search
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls='navbar' />
-                    <Navbar.Collapse id='navbar'>
-                        <Nav className='nav-links'>
-                            <Nav.Link block style={{color: '#1ACDC8'}} as={Link} to='/'>
+  return (
+    <>
+      <Navbar block style={{ backgroundColor: "#217DA8" }} expand="lg">
+        <Container fluid>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            block
+            style={{ color: "#1ACDC8", fontWeight: "bold" }}
+          >
+            Movie Mania Search
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar" />
+          <Navbar.Collapse id="navbar">
+            <Nav className="nav-links">
+              {/* <Nav.Link block style={{color: '#1ACDC8'}} as={Link} to='/'>
                                 Search For Movies
-                            </Nav.Link>
-                            {Auth.loggedIn() ? (
-                                <>
-                                    <Nav.Link block style={{color: '#1ACDC8'}} as={Link} to='/saved'>
-                                        See Your Watchlist
-                                    </Nav.Link>
-                                    <Nav.Link block style={{color: '#1ACDC8'}} onClick={Auth.logout}>Logout</Nav.Link>
-                                </>
-                            ) : (
-                                    <Nav.Link block style={{color: '#1ACDC8'}} onClick={() => setDisplayModal(true)}>Login or Sign Up</Nav.Link>
-                                )}
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-            <Modal
-                size='lg'
-                show={displayModal}
-                onHide={() => setDisplayModal(false)}
-                aria-labelledby='signup-modal'>
-                <Tab.Container defaultActiveKey='login'>
-                    <Modal.Header closeButton>
-                        <Modal.Title id='signup-modal'>
-                            <Nav variant='pills' style={{backgroundColor: '#96F2EF', color: '#217DA8', fontWeight: 'bold'}}>
-                                <Nav.Item>
-                                    <Nav.Link eventKey='login'>Login</Nav.Link>
-                                </Nav.Item>
-                                
-                                <Nav.Item>
-                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Tab.Content>
-                            <Tab.Pane eventKey='login'>
-                                <LoginForm handleModalClose={() => setDisplayModal(false)} />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey='signup'>
-                                <SignUpForm handleModalClose={() => setDisplayModal(false)} />
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Modal.Body>
-                </Tab.Container>
-            </Modal>
-        </>
-    );
+                            </Nav.Link> */}
+              {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link block style={{ color: "#1ACDC8" }} as={Link} to="/">
+                    Search For Movies
+                  </Nav.Link>
+                  <Nav.Link
+                    block
+                    style={{ color: "#1ACDC8" }}
+                    as={Link}
+                    to="/saved"
+                  >
+                    See Your Watchlist
+                  </Nav.Link>
+                  <Nav.Link
+                    block
+                    style={{ color: "#1ACDC8" }}
+                    onClick={Auth.logout}
+                  >
+                    Logout
+                  </Nav.Link>
+                </>
+              ) : (
+                <Nav.Link
+                  block
+                  style={{ color: "#1ACDC8" }}
+                  onClick={() => setDisplayModal(true)}
+                >
+                  Login or Sign Up
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Modal
+        size="lg"
+        show={displayModal}
+        onHide={() => setDisplayModal(false)}
+        aria-labelledby="signup-modal"
+      >
+        <Tab.Container defaultActiveKey="login">
+          <Modal.Header closeButton>
+            <Modal.Title id="signup-modal">
+              <Nav
+                variant="pills"
+                style={{
+                  backgroundColor: "#96F2EF",
+                  color: "#217DA8",
+                  fontWeight: "bold",
+                }}
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="login">Login</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="login">
+                <LoginForm handleModalClose={() => setDisplayModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey="signup">
+                <SignUpForm handleModalClose={() => setDisplayModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
+    </>
+  );
 };
 
-export default AppNavbar
+export default AppNavbar;
