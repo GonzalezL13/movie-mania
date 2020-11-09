fc77894baa464b0ca5bd66b8d467f3af8d4940a2
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-11-08 17:47:49 -0700

    uncommented code on index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,8 +8,8 @@ router.use((req, res) => {
   res.status(404).send("<h1>😝 404 Error!</h1>");
 });
 
-// router.use((req, res) =>
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
