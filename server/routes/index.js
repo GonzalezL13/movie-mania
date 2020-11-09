4475172dac40cbda1a61126628d0ed6fbe5e77af
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-11-08 17:55:33 -0700

    uncommented code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-// const path = require('path');
+const path = require('path'); 
 const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+//return 404
 router.use((req, res) => {
   res.status(404).send("<h1>😝 404 Error!</h1>");
 });
